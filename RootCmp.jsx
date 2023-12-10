4955d118b5a82d4441aa0bb18a9c16438a1f4637
DimRev <dimarev444@gmail.com>
2023-12-10 16:27:49 +0200

    Add : Page nav

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -4,21 +4,27 @@ import { Home } from './pages/Home.jsx'
 import { BookIndex } from './pages/BookIndex.jsx'
 import { bookService } from './services/book.service.js'
 
+const { useState, useEffect } = React
 export function App() {
+  const [currentPage, setCurrentPage] = useState('home')
+
+  function onPageChange(page) {
+    setCurrentPage(page)
+  }
   return (
     <React.Fragment>
       <section className="app-header">
         <div className="logo">MissBooks</div>
         <nav className="pages-nav">
-          <a href="#">Home</a>
-          <a href="#">About us</a>
-          <a href="#">Books</a>
+          <a href="#" onClick={()=>onPageChange('home')}>Home</a>
+          <a href="#" onClick={()=>onPageChange('about')}>About us</a>
+          <a href="#" onClick={()=>onPageChange('books')}>Books</a>
         </nav>
       </section>
       <main>
-        <Home />
-        <About />
-        <BookIndex />
+      {currentPage==='home' && <Home /> }
+      {currentPage==='about' && <About /> }
+      {currentPage==='books' && <BookIndex /> }
         <BookDetails />
       </main>
     </React.Fragment>
