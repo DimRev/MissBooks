971f2b93af2ca3bda5d0d8c514da7a345d5e9fbf
DimRev <dimarev444@gmail.com>
2023-12-10 16:14:09 +0200

    Refactor : Home and RootCmp

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,28 +1,23 @@
 import { Home } from './pages/Home.jsx'
+import { BookIndex } from './pages/BookIndex.jsx'
 import { bookService } from './services/book.service.js'
 
 export function App() {
-    return (
-        <React.Fragment>
-            <section className="app-header">
-                <div className="logo">
-                    MissBooks
-                </div>
-                <nav className="pages-nav">
-                    <a href="#">Home</a>
-                    <a href="#">About us</a>
-                    <a href="#">Books</a>
-                </nav>
-            </section>
-            <main>
-                <Home />
-                <section className="about-us-page">
-
-                </section>
-                <section className="books-page">
-
-                </section>
-            </main>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <section className="app-header">
+        <div className="logo">MissBooks</div>
+        <nav className="pages-nav">
+          <a href="#">Home</a>
+          <a href="#">About us</a>
+          <a href="#">Books</a>
+        </nav>
+      </section>
+      <main>
+        <Home />
+        <section className="about-us-page"></section>
+        <BookIndex />
+      </main>
+    </React.Fragment>
+  )
 }
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,8 @@
 import { UserPreview } from '../cmps/UserPreview.jsx'
-import { bookService } from '../services/book.service.js'
 
 const { useEffect } = React
 
 export function Home() {
-    useEffect(()=> {
-        bookService.query().then((books) => {console.log(books)})
-    },[])
 
     return (
         <section className='home-page'>
