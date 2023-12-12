3557f86f37127c572920a423f21063515139636f
DimRev <dimarev444@gmail.com>
2023-12-12 08:04:36 +0200

    Refactor : Function to format the price

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -5,14 +5,16 @@ export function BookPreview({ book }) {
   const thumbnail = book.thumbnail
   const isOnSale = book.listPrice.isOnSale
 
-  const priceAmount = book.listPrice.amount
-  const pricecurrencyCode = book.listPrice.currencyCode
-  const priceFormatter = new Intl.NumberFormat('he-IL', {
-    style: 'currency',
-    currency: pricecurrencyCode,
-  })
-  const price = priceFormatter.format(priceAmount)
-
+  function getFormattedPrice() {
+    const priceAmount = book.listPrice.amount
+    const pricecurrencyCode = book.listPrice.currencyCode
+    const priceFormatter = new Intl.NumberFormat('he-IL', {
+      style: 'currency',
+      currency: pricecurrencyCode,
+    })
+    return priceFormatter.format(priceAmount)
+  }
+  
   function isOnSaleClass(isOnSale) {
     if (isOnSale) {
       return 'sale-book'
@@ -26,7 +28,7 @@ export function BookPreview({ book }) {
       <h2 className="book-title">{title}</h2>
       <h2 className="book-authors">{authors}</h2>
       <h2 className="book-date">{publishedDate}</h2>
-      <h2 className="book-price">{price}</h2>
+      <h2 className="book-price">{getFormattedPrice()}</h2>
       <img src={thumbnail} alt="test" />
     </article>
   )
